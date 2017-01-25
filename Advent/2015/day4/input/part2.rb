ed1dac26f97b119ae6a562fff3f22e3ff2980bba
Roman A Davis <FluffyRibbit@gmail.com>
2017-01-25 08:39:49 -0500

    Advent

--- Advent/2015/day4/input/part2.rb
+++
@@ -1,7 +0,0 @@
-require "digest"
-
-def coinmine(string, zeroes = 5, iterator = 0)# Can't resist golfing
-  number += 1 until Digest::MD5.hexdigest(string + number.to_s)[0..zeroes] == "0" * zeroes
-end
-
-#puts coinmine
---
+++ Advent/2015/day5/solutions/part1.rb
@@ -0,0 +1,31 @@
+# Santa needs help figuring out which strings in his text file are naughty or
+# nice.
+#
+# A nice string is one with all of the following properties:
+#
+#     It contains at least three vowels (aeiou only), like aei, xazegov, or
+#     aeiouaeiouaeiou.
+#     It contains at least one letter that appears twice in a row, like xx,
+#     abcdde (dd), or aabbccdd (aa, bb, cc, or dd).
+#     It does not contain the strings ab, cd, pq, or xy, even if they are part
+#     of one of the other requirements.
+
+class string
+  def nice?
+    return false unless vowels_at_least?(3)
+    return false unless repeating_letters?
+    return !naughty_substrings?
+  end
+
+  def vowels_at_least?(n)
+    scan(/[aeiou]/).count >= n
+  end
+
+  def repeating_letters?
+    match(/(.)\1{1,}/)
+  end
+
+  def naughty_substrings?
+    match(/ab|cd|pq|xy/)
+  end
+end
