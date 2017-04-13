80e8371f120ee20d8d91fa0f7407413d66b06ce4
Roman A. Davis <FluffyRibbit@gmail.com>
2017-04-13 16:17:49 -0400

    Finished hamming in Ruby for exercism. Fetched gigasecond.

--- exercism/ruby/hamming/hamming.rb
+++ exercism/ruby/hamming/hamming.rb
@@ -1,9 +1,10 @@
 class Hamming
-  def compute(a, b)
-    distance = 0
-    a.chars.with_index do |char, index|
-      distance += 1 if char != b[index]
-    end
-    distance
+  def self.compute(a, b)
+    raise(ArgumentError) if a.length != b.length
+    a.chars.each.with_index.count { |char, index| char != b[index] }
   end
+end
+
+module BookKeeping
+  VERSION = 3
 end
