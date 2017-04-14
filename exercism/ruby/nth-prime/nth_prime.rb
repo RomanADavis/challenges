16678000b32a7eff65a3f7d81f6bdae1f6f8395a
Roman A. Davis <FluffyRibbit@gmail.com>
2017-04-13 22:39:11 -0400

    Work on nth prime. Probably going to bed soon.

---
+++ exercism/ruby/nth-prime/nth_prime.rb
@@ -0,0 +1,19 @@
+class Prime
+  def self.nth(n)
+    raise ArgumentError if n < 1
+    primes = []
+    candidate = 2 # 1 is not prime
+
+    while primes.length < n
+      p primes
+      primes << candidate unless primes.any? {|prime| candidate % prime == 0}
+      candidate += 1
+    end
+
+    primes.last
+  end
+end
+
+module BookKeeping
+  VERSION = 1
+end
