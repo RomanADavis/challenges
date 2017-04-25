04f86e7286940744741d075f7bb87b2c4e05dfe6
Roman A. Davis <FluffyRibbit@gmail.com>
2017-04-24 22:05:01 -0400

    Pass all alphmetic tests in Ruby for Exercism, but it's a little slow.

--- exercism/ruby/alphametics/alphametics.rb
+++ exercism/ruby/alphametics/alphametics.rb
@@ -9,13 +9,14 @@ class Alphametics
   end
 
   private
-    def self.try_solutions
+    def self.try_solutions # Brute force. Slow at ten letters.
       @solutions.each do |combo|
         combo.each do |permutation|
           solution = attempt(permutation.to_a)
           return solution if solved?(solution)
         end
       end
+      {}
     end
 
     def self.attempt(numbers)
@@ -24,9 +25,13 @@ class Alphametics
       result
     end
 
-    def self.solved?(attempt)
+    def self.solved?(attempt) #It might be better if I wrote a small interpreter for this.
       fillin = @puzzle.chars.map {|char| attempt.fetch(char, char)}.join
-      return false if fillin.scan(/\d+/).any? {|n| n[0] == "0"}
+      return false if fillin.scan(/\d+/).any? {|n| n[0] == "0"} # For octals
       eval(fillin)
     end
+end
+
+module BookKeeping
+  VERSION = 4
 end
