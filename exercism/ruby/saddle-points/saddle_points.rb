af3de7a70cb2503c9d9f693901759f528c2c4fb2
Roman A. Davis <FluffyRibbit@gmail.com>
2017-04-28 06:19:56 -0400

    Passed all saddle point tests in Ruby for Exercism

--- exercism/ruby/saddle-points/saddle_points.rb
+++ exercism/ruby/saddle-points/saddle_points.rb
@@ -1,11 +1,42 @@
 class Matrix
-  attr_accessor :rows, :columns
+  attr_accessor :string
   def initialize(string)
-    rows = string.split("\n")
-    self.rows = rows.map {|row| row.split.map(&:to_i)}
+    self.string = string
+  end
 
-    self.columns = (0...rows.length).map do |index|
-      self.rows.map {|row| row[index]}
+  def rows
+    return @rows if @rows
+
+    rows = string.split("\n")
+    rows.map {|row| row.split.map(&:to_i)}
+  end
+
+  def columns
+    return @columns if @columns
+
+    length = rows.first.length
+
+    @columns = (0...length).map do |index|
+      rows.map {|row| row[index]}
+    end
+  end
+
+  def saddle_points
+    return @saddle if @saddle
+    highest = []
+
+    # Get the index of each part of the matrix with the highest value in row
+    rows.each_with_index do |row, y|
+      row.each_with_index do |val, x|
+        highest << [y, x] if row.max == val
+      end
+    end
+
+    # Select from the highest that are also the lowest in their column
+    @saddle = highest.select do |high|
+      y = high.first
+      x = high.last
+      rows[y][x] == columns[x].min
     end
   end
 end
