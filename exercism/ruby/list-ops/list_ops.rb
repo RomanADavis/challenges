eb7f4abf00e0dfbd4c07cfbf54f4fca66e74e133
Roman A. Davis <FluffyRibbit@gmail.com>
2017-05-05 15:55:59 +0800

    Passed all list op tests in Ruby for Exercism

--- exercism/ruby/list-ops/list_ops.rb
+++ exercism/ruby/list-ops/list_ops.rb
@@ -1,6 +1,54 @@
 class ListOps
-  def self.arrays(array) # this is such a terrible name for what this does
-    array.inject(0) {|total| total + element}
+  def self.arrays(array)
+    array.length
   end
-  
+
+  def self.eacher(array)
+    arrays(array).times do |iterator|
+      item = array[iterator]
+      yield(item)
+    end
+  end
+
+  def self.reverser(array)
+    result = []
+    eacher(array) {|element| result.unshift(element)}
+    result
+  end
+
+  def self.concatter(first_half, second_half)
+    result = first_half.dup
+    eacher(second_half) {|element| result << element}
+    result
+  end
+
+  def self.mapper(array)
+    result = []
+    eacher(array) {|item| result << yield(item)}
+    result
+  end
+
+  def self.filterer(array)
+    result = []
+    eacher(array) {|item| result << item if yield(item)}
+    result
+  end
+
+  def self.injector(array, start = 0)
+    total = start
+    eacher(array) {|element| total = yield(total, element)}
+    total
+  end
+
+  def self.sum_reducer(array)
+    injector(array) {|total, element| total + element}
+  end
+
+  def self.factorial_reducer(array)
+    injector(array, 1) {|total, element| total * element}
+  end
+end
+
+module BookKeeping
+  VERSION = 2
 end
