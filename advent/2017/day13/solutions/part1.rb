945943f9b57930543e4e4b9dceb3aa4c3f788d0b
Roman A. Davis <FluffyRibbit@gmail.com>
2017-12-13 23:41:32 +0800

    Extracted out parts of construct into Layer's new image method, among other things for day 13 of advent 2017

--- advent/2017/day13/solutions/part1.rb
+++ advent/2017/day13/solutions/part1.rb
@@ -7,23 +7,29 @@ class Layer
     self.orientation = :forward
   end
 
-  def tick
+  def update
     if self.orientation == :forward
       self.scanner_position += 1
-      self.orientation = :backward unless self.scanner_position == self.range
+      self.orientation = :backward unless self.scanner_position < self.range
     else
       self.scanner_position -= 1
-      self.orientation = :forward unless self.scanner_position == 1
+      self.orientation = :forward unless self.scanner_position > 1
     end
+  end
+
+  def image
+    wall = Array.new(self.range + 1) {"[ ]"}
+    wall[0] = " #{self.depth} "
+    wall[self.scanner_position] = "[S]"
+    wall
   end
 end
 
 class Firewall
-  attr_accessor :layers, :firewall
+  attr_accessor :layers
 
   def initialize(filename)
     self.layers = parse(File.readlines(filename))
-    self.firewall = construct
   end
 
   def parse(lines)
@@ -32,21 +38,22 @@ class Firewall
     end
   end
 
-  def construct
+  def image
     # Set the parameters of the grid
     firewall = Array.new(self.layers.max_by(&:depth).depth + 1) do
       Array.new(self.layers.max_by(&:range).range + 1) {"   "}
     end
 
-    # Draw the wall
+    # Label layers and draw gaps
     firewall.each_with_index do |line, index|
       line[0] = " #{index} "
       line[1] = "..."
-    end# No wall hereo
+    end#
+
+    # Draw walls
     self.layers.each do |layer|
-      firewall[layer.depth][1] = "[S]" # Place the scanner
-      firewall[layer.depth][2..layer.range].each_with_index do |_, i|
-        firewall[layer.depth][i + 2] = "[ ]"
+      layer.image.each_with_index do |cell, index|
+        firewall[layer.depth][index] = cell
       end
     end
 
@@ -55,15 +62,27 @@ class Firewall
   end
 
   def draw
-    firewall.each do
+    image.each do
       |line| line.each {|cell| print cell, ' '}
       puts "\n"
     end
   end
 
-  def tick
+  def update
+    self.layers.each {|layer| layer.update}
+  end
 
+  def tick
+    draw
+    gets
+    update
+  end
+
+  def run
+    loop do
+      tick
+    end
   end
 end
 
-Firewall.new("../input/sample.txt").draw
+Firewall.new("../input/sample.txt").run
