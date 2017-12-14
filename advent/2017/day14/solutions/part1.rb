93c26269067e85f388cd0f2a5f03bef76d276dc8
Roman A. Davis <FluffyRibbit@gmail.com>
2017-12-14 16:47:23 +0800

    Copied over code for the KnotHash object from day 10 of advent of code to here for easier usage.

---
+++ advent/2017/day14/solutions/part1.rb
@@ -0,0 +1,68 @@
+class KnotHash
+  attr_accessor :content, :position, :lengths, :skip_size
+
+  def initialize(input, tail = 255)
+    self.lengths = salt(input)
+    self.content = (0..tail).to_a
+    self.position = 0
+    self.skip_size = 0
+  end
+
+  def salt(input)
+    input.chars.map(&:ord) + [17, 31, 73, 47, 23]
+  end
+
+  def sparse_hash
+    64.times {twist_all}
+
+    self.content
+  end
+
+  def dense_hash
+    sparse_hash.each_slice(16).map {|block| block.inject(:^)}
+  end
+
+  def generate
+    dense_hash.map {|digit| digit.to_s(16).rjust(2, '0')}.join
+  end
+
+  def checksum
+    # p self.content
+    twist_all
+
+    self.content[0..1].inject(:*)
+  end
+
+  def twist_all
+    self.lengths.each do |length|
+      twist(length)
+      self.position = (self.position + length + self.skip_size) % self.content.length
+      self.skip_size += 1
+    end
+  end
+
+  def reverse_subsection(length)
+    tail = self.position + length
+    mirror = self.content[position..(tail - 1)] || []
+
+    if mirror.length < length
+      mirror += self.content[0..(length - mirror.length - 1)]
+    end
+
+    mirror = mirror.reverse
+  end
+
+  def twist(length)
+    mirror = reverse_subsection(length)
+
+    self.content.each_with_index do |_, index|
+      self.content[index] = mirror.shift if index >= position && mirror[-1]
+    end
+
+    mirror.each_with_index do |value, index|
+      self.content[index] = value
+    end
+
+    self.content
+  end
+end
