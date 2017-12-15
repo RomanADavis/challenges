278a32a6cda9abaef8f268960dc9a7f7214fb0b5
Roman A. Davis <FluffyRibbit@gmail.com>
2017-12-15 17:33:07 +0800

    Advent 2017, Day 15, Part 2: Acquired the second star. Code is a tad slow, but still performs in under a minute.

--- advent/2017/day15/solution/part2.rb
+++ advent/2017/day15/solution/part2.rb
@@ -1,14 +1,15 @@
 class Generator
   attr_accessor :seed, :factor, :criteria
   def initialize(seed, factor, criteria)
-    self.seed   = seed
-    self.factor = factor
+    self.seed     = seed
+    self.factor   = factor
     self.criteria = criteria
   end
 
   def next
-    until self.seed % self.criteria == 0
+    loop do
       self.seed = self.seed * self.factor % 2147483647
+      break if self.seed % self.criteria == 0
     end
 
     self.seed
@@ -23,13 +24,12 @@ class Judge
   end
 
   def match? # Check if the last sixteen bits of the next seed match for all generators
-    generators.map do |generator|
-      generator.next #& 0xFFFF
-    end #.uniq.length == 1
+    generators.map {|generator| generator.seed & 0xFFFF}.uniq.length == 1
   end
 
   def round
-    p self.generators
+    self.generators.each {|generator| generator.next}
+    # self.generators.each {|generator| p generator.seed}
     self.matches += 1 if match?
   end
 
@@ -40,4 +40,7 @@ class Judge
   end
 end
 
-p Judge.new(Generator.new(65, 16807, 4), Generator.new(8921, 48271, 8)).tally(5)
+# p Judge.new(Generator.new(65, 16807, 4), Generator.new(8921, 48271, 8)).tally(5_000_000)
+# => 509
+
+p Judge.new(Generator.new(289, 16807, 4), Generator.new(629, 48271, 8)).tally(5_000_000)
