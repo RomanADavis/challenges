3907578721187ed38428d1a504972df1d7485bdf
Roman A. Davis <FluffyRibbit@gmail.com>
2017-12-17 01:28:17 +0800

    Minor refactoring to part1 of day16 for advent of code 2017

--- advent/2017/day16/solutions/part1.rb
+++ advent/2017/day16/solutions/part1.rb
@@ -1,19 +1,19 @@
 class DanceLine
   attr_accessor :programs, :moves, :operations
+
+  spin = ->(x) {self[0..-1] = self[-x.to_i..-1] + self[0..(-x.to_i - 1)]}
+  exchange = ->(a, b) {self[a.to_i], self[b.to_i] = self[b.to_i], self[a.to_i]}
+  partner = ->(a, b) {self[find_index(a)], self[find_index(b)] = b, a}
+
+  @@operations = {'s' => spin,'x' => exchange,'p' => partner}
+
   def initialize(programs, dance_moves)
     self.programs = programs
     self.moves = dance_moves.split(',')
-
-    spin = ->(x) {self[0..-1] = self[-x.to_i..-1] + self[0..(-x.to_i - 1)]}
-    exchange = ->(a, b) {self[a.to_i], self[b.to_i] = self[b.to_i], self[a.to_i]}
-    partner = ->(a, b) {self[find_index(a)], self[find_index(b)] = b, a}
-
-    self.operations = {'s' => spin,'x' => exchange,'p' => partner}
   end
 
   def execute(move)
-    args, operation = move[1..-1].split('/'), self.operations[move[0]]
-    self.programs.instance_exec(*args, &operation)
+    self.programs.instance_exec(*move[1..-1].split('/'), &@@operations[move[0]])
   end
 
   def run
