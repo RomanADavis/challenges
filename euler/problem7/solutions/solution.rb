3443e9931fa317ab66aa74adbe9ccf5dd133abb7
Roman A. Davis <FluffyRibbit@gmail.com>
2017-12-17 18:37:55 +0800

    Solved problem 7 for Project Euler

---
+++ euler/problem7/solutions/solution.rb
@@ -0,0 +1,17 @@
+def prime(n) # get the nth prime number
+  primes = [2]
+  candidate = 3
+
+  until primes.length == n
+    # Only check candidate against primes under the square root of the candidate
+    factors = primes[0..primes.find_index {|prime| prime > candidate ** 0.5}]
+    primes << candidate if factors.all? {|prime| candidate % prime != 0}
+    candidate += 2 # Only odd numbers after 2 can be prime.
+  end
+
+  primes.max
+end
+
+p prime(6)
+
+p prime(10_001)
