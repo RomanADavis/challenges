e84496622368be396fa839a634a27a174847c830
Roman A. Davis <FluffyRibbit@gmail.com>
2017-12-20 19:09:25 +0800

    Solved problem 46 for Project Euler

---
+++ euler/problem46/solutions/solution.rb
@@ -0,0 +1,29 @@
+def prime?(n)
+  return false if n == 1
+  return true  if n == 2
+  return false if n % 2 == 0
+
+  (3..(n**0.5).to_i).step(2) do |factor|
+    return false if n % factor == 0
+  end
+
+  true
+end
+
+def square?(n)
+  n**0.5 % 1 == 0
+end
+
+def conjecture?(n)
+  return true if prime?(n) # Conjecture only applies to composites
+  s = 1
+  until (s**2) * 2 > n
+    return true if prime?(n - s**2 * 2)
+    s += 1
+  end
+  false
+end
+
+n = 35
+n += 2 while conjecture?(n)
+p n
