161a280624da5e683a73d6c8e79445f8503d09d4
Roman A. Davis <FluffyRibbit@gmail.com>
2017-12-24 22:01:49 +0800

    Some work on problem 51 for Project Euler

---
+++ euler/problem51/solutions/solution.rb
@@ -0,0 +1,37 @@
+class Replaceable
+  attr_accessor :string, :values
+  def initialize(string)
+    self.string = string
+    self.values = parse
+  end
+
+  def parse
+    values = 10.times.map do |i|
+      self.string.gsub('r', i.to_s).to_i
+    end
+
+    values.reject {|value| value.to_s.length != values[-1].to_s.length}
+  end
+end
+
+class NonReplaceable
+  attr_accessor :string, :values
+  def initialize(string)
+    self.string = string
+    self.values = parse
+  end
+
+  def parse
+    count = 10**self.string.count('n')
+    bottom = 10**(self.string.count('n') - 1)
+
+    (bottom...count).map do |n|
+      n = n.to_s
+      string = self.string
+      n.length.times do |index|
+        string = string.sub('n', n[index])
+      end
+      Replaceable.new(string).values
+    end
+  end
+end
