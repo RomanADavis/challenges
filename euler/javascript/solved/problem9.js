69f57160a2e354738cd750052374a9ec7999babc
Roman Alexander Davis <fluffyribbit@gmail.com>
2019-11-03 07:09:55 +0800

    tried using euler's formula for problem9 in js

--- euler/javascript/solved/problem9.js
+++ euler/javascript/solved/problem9.js
@@ -18,5 +18,26 @@ function pythagoreanTriplet(total){
   return -1; //  None found
 }
 
+function integer(number){
+  return Math.floor(number) === number;
+}
+
+function whole(number){
+  return integer(number) && number >= 0;
+}
+
+function euclidsTriplet(total){ // TODO: Add k
+  // by Euclid's Formula (a + b + c / 2) = m (m + n)
+  // m must be greater than n because a = m^2 - n^2, and a can't be negative
+  for(let m = Math.ceil(Math.sqrt(total / 2) / 2); m * m < (total / 2); m++){
+    let n = (total / 2 / m) - m;
+    let a = m * m - n * n, b = 2 * m * n, c = m * m + n * n;
+    if(whole(a) && whole(b) && whole(c)){
+      return [a, b, c];
+    }
+  }
+  return -1; // None found.
+}
 console.log(`The only pythagorean triple where a + b + c = 1000 is ${pythagoreanTriplet(1000)}.`);
 console.log(`Their product is ${pythagoreanTriplet(1000).reduce((total, side) => total * side)}`);
+console.log(`You can also use Euclid's Formula to make this faster: ${euclidsTriplet(1000)}`);
